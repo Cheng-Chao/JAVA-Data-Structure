8be8b300dddba9c440eddfc85ffbaa6d49e56d6d
coderlover <823292004@qq.com>
2016-11-19 23:30:40 +0800

    添加栈的单链表实现

---
+++ src/main/java/Stack.java
@@ -0,0 +1,77 @@
+/**
+ * @author chengchao
+ * @time 2016/11/19
+ * 栈 -> 单链表实现
+ */
+public class Stack {
+    //栈的大小
+    public int size;
+    //构造函数
+    Stack(int data){
+        this.size = data;
+    }
+
+    LinkedList linkedList = new LinkedList();
+
+    /**
+     * 入栈
+     */
+    public void add(int data){
+        if(isFull()){
+            System.out.println("Add failed!!!The Stack will overflow!!!");
+        }
+        else {
+            linkedList.addFromStart(data);
+        }
+    }
+
+    /**
+     * 出栈
+     */
+    public int popUp(){
+        if(this.isEmpty()){
+            System.out.println("Popup failed!!!The Stack is empty now!!!");
+            return -1;
+        }else{
+            int currentData = linkedList.first.data;
+            linkedList.deleteFromStart();
+            return  currentData;
+        }
+    }
+
+    /**
+     * 判断是否是空栈
+     */
+    public boolean isEmpty(){
+        boolean result = false;
+        if(linkedList.first == null){
+            result = true;
+        }
+        return  result;
+    }
+
+    /**
+     * 判断是否是满栈
+     */
+    public boolean isFull(){
+        boolean result = false;
+        if(linkedList.length() == this.size){
+            result = true;
+        }
+        return result;
+    }
+
+    /**
+     * 输出所有栈数据
+     */
+    public void printAll(){
+        linkedList.printAll();
+    }
+
+    /**
+     * 栈的当前数据个数
+     */
+    public int length(){
+        return linkedList.length();
+    }
+}
---
+++ src/test/java/StacktTest.java
@@ -0,0 +1,35 @@
+/**
+ * @author chengchao
+ * @time 2016/11/19
+ * 栈测试
+ */
+import org.junit.Before;
+import org.junit.Test;
+
+public class StacktTest {
+    Stack stack;
+
+    @Before
+    public void prepare(){
+        stack = new Stack(5);
+    }
+
+    @Test
+    public void test(){
+        stack.add(1);
+        stack.add(3);
+        stack.add(5);
+        stack.add(7);
+        stack.add(9);
+        stack.add(11);
+        stack.printAll();
+        System.out.println(stack.length());
+        System.out.println(stack.popUp());
+        stack.printAll();
+        System.out.println(stack.length());
+        stack.add(27);
+        stack.printAll();
+        stack.popUp();
+        stack.printAll();
+    }
+}
